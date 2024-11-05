692d9191141c255c8d30412eaf44b089aa438fea
Victor Berdugo <vberdugo@car8s5.42barcelona.com>
2024-11-05 12:01:56 +0100

    new

---
+++ get_next_line.c
@@ -0,0 +1,59 @@
+#include "get_next_line.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (src[i])
+	{
+	   dest[i] = src[i];
+	   i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		line[70000];
+	static int	buffer_read;
+	static int 	buffer_pos;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buffer_pos >= buffer_read)
+		{
+			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_read <= 0)
+				break ;
+		}
+		if (line[i] == '\n')
+			break ;
+		line[i] = buffer[buffer_pos++];
+		i++;
+	}
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
+/*
+int main()
+{
+    int fd = open("./txt.txt", O_RDONLY);
+    printf("%s", get_next_line(fd));
+    return (0);
+}
+*/
